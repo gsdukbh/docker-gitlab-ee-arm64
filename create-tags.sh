423550b0187559a5e3d8ba35d9fabc8a6cd2bc2b
JiaWei Lee <gsdukbh@gmail.com>
2022-04-30 01:24:03 +0800

    add

--- create-tags.sh
+++ create-tags.sh
@@ -10,22 +10,32 @@ cat version_list | while read version; do
     done
     if ! ${HAVE_TAG}; then
         git tag ${version}
-        # get omnibus
-        sudo git clone https://gitlab.com/gitlab-org/omnibus-gitlab.git
-        cd omnibus-gitlab/docker
-        echo "PACKAGECLOUD_REPO=gitlab-ee" > RELEASE 
-        echo "RELEASE_PACKAGE=gitlab-ee" >> RELEASE
-        echo "RELEASE_VERSION=${version}" >> RELEASE
-        echo "DOWNLOAD_URL=https://packages.gitlab.com/gitlab/gitlab-ee/packages/ubuntu/focal/gitlab-ee_${version}_arm64.deb/download.deb" >> RELEASE
-        sed -i 's/\-recommends/\-recommends libatomic1/' Dockerfile
-        sudo docker buildx  build --platform linux/arm64 -t ${DOCKER_NAME}/gitlab-ee-arm64:${version} -f Dockerfile ./ 
-        cd ../../
-        LATEST=$(git tag | sort -rV | head -n 1)
-        if [ "${LATEST}" == "${version}" ]; then 
-             docker tag ${DOCKER_NAME}/gitlab-ee-arm64:${version} ${DOCKER_NAME}/gitlab-ee-arm64:latest;
-        fi
-        docker login --username ${DOCKER_NAME} --password ${DOCKER_PASSWORD} 
-        docker push -a ${DOCKER_NAME}/gitlab-ee-arm64
-
     fi
 done
+
+LATEST=$(git tag | sort -rV | head -n 1)
+
+if [ -f ./latest ]; then
+    old= cat ./latest
+else
+    echo LATEST > latest
+    old =l
+fi
+
+if  test $old != LATEST; then
+    echo LATEST > latest
+    sudo git clone https://gitlab.com/gitlab-org/omnibus-gitlab.git
+    cd omnibus-gitlab/docker
+    echo "PACKAGECLOUD_REPO=gitlab-ee" > RELEASE 
+    echo "RELEASE_PACKAGE=gitlab-ee" >> RELEASE
+    echo "RELEASE_VERSION=${LATEST}" >> RELEASE
+    echo "DOWNLOAD_URL=https://packages.gitlab.com/gitlab/gitlab-ee/packages/ubuntu/focal/gitlab-ee_${LATEST}_arm64.deb/download.deb" >> RELEASE
+    sed -i 's/\-recommends/\-recommends libatomic1/' Dockerfile
+    sudo docker buildx  build --platform linux/arm64 -t ${DOCKER_NAME}/gitlab-ee-arm64:${LATEST} -f Dockerfile ./ 
+    cd ../../
+    docker tag ${DOCKER_NAME}/gitlab-ee-arm64:${LATEST} ${DOCKER_NAME}/gitlab-ee-arm64:latest;
+    docker login --username ${DOCKER_NAME} --password ${DOCKER_PASSWORD} 
+    docker push -a ${DOCKER_NAME}/gitlab-ee-arm64
+    git add latest
+    git commit -m "build version ${LATEST}"
+fi
