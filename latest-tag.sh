08c952479bedbbf4f48e07c1c58722ed2442302f
JiaWei Lee <gsdukbh@gmail.com>
2022-04-29 22:49:32 +0800

    add

--- latest-tag.sh
+++ latest-tag.sh
@@ -2,6 +2,7 @@
 
 LATEST=$(git tag | sort -rV | head -n 1)
 echo ${LATEST}
+git tag -a ${LATEST} -m "latest version"
 # if [ "${LATEST}" == "${CI_COMMIT_TAG}" ]; then 
 #     docker tag ${DOCKER_NS}/${BASENAME}:${CI_COMMIT_TAG} ${DOCKER_NS}/${BASENAME}:latest;
 # fi
