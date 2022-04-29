52c989d2efc9362fcb1171d7d4acce02e577bcc7
JiaWei Lee <gsdukbh@gmail.com>
2022-04-30 00:54:04 +0800

    add

--- check-version.sh
+++ check-version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SEARCH_PAGES='2'
+SEARCH_PAGES='1'
 
 if [ -f ./version_list ]; then
     rm version_list
@@ -10,3 +10,4 @@ for i in $(seq 1 ${SEARCH_PAGES}); do
     curl -s "https://packages.gitlab.com/gitlab/gitlab-ee?page=${i}" | grep "_arm64.deb" | grep -v '\-rc' | sed 's/.*>\(.*\)<.*/\1/' | sort -u | sed 's/gitlab-ee_\(.*\)_arm64.deb/\1/' >> version_list;
 done
 sort -rVu version_list -o version_list
+
--- create-tags.sh
+++ create-tags.sh
@@ -10,8 +10,22 @@ cat version_list | while read version; do
     done
     if ! ${HAVE_TAG}; then
         git tag ${version}
+        # get omnibus
+        sudo git clone https://gitlab.com/gitlab-org/omnibus-gitlab.git
+        cd omnibus-gitlab/docker
+        echo "PACKAGECLOUD_REPO=gitlab-ee" > RELEASE 
+        echo "RELEASE_PACKAGE=gitlab-ee" >> RELEASE
+        echo "RELEASE_VERSION=${version}" >> RELEASE
+        echo "DOWNLOAD_URL=https://packages.gitlab.com/gitlab/gitlab-ee/packages/ubuntu/focal/gitlab-ee_${version}_arm64.deb/download.deb" >> RELEASE
+        sed -i 's/\-recommends/\-recommends libatomic1/' Dockerfile
+        sudo docker buildx  build --platform linux/arm64 -t ${DOCKER_NAME}/gitlab-ee-arm64:${version} -f Dockerfile ./ 
+        cd ../../
+        LATEST=$(git tag | sort -rV | head -n 1)
+        if [ "${LATEST}" == "${version}" ]; then 
+             docker tag ${DOCKER_NAME}/gitlab-ee-arm64:${version} ${DOCKER_NAME}/gitlab-ee-arm64:latest;
+        fi
+        docker login --username ${DOCKER_NAME} --password ${DOCKER_PASSWORD} 
+        docker push -a ${DOCKER_NAME}/gitlab-ee-arm64
+
     fi
 done
-if [ "${HAVE_TAG}" == "true" ];then
-    echo "hhhhhhhh"
-fi
