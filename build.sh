3c5e924b8f5acc217b972704d0dd244291dfe5d8
JiaWei Lee <gsdukbh@gmail.com>
2022-04-30 01:56:13 +0800

    add

--- build.sh
+++ build.sh
@@ -31,13 +31,13 @@ if  test $old != LATEST ; then
     echo "RELEASE_VERSION=${LATEST}" >> RELEASE
     echo "DOWNLOAD_URL=https://packages.gitlab.com/gitlab/gitlab-ee/packages/ubuntu/focal/gitlab-ee_${LATEST}_arm64.deb/download.deb" >> RELEASE
     sed -i 's/\-recommends/\-recommends libatomic1/' Dockerfile
-    docker buildx  build --platform linux/arm64 -t ${{secrets.DOCKER_NAME}}/gitlab-ee-arm64:${LATEST} -f Dockerfile ./ 
+    docker buildx  build --platform linux/arm64 -t ${DOCKER_NAME}/gitlab-ee-arm64:${LATEST} -f Dockerfile ./ 
     cd ../../
-    docker tag ${{secrets.DOCKER_NAME}}/gitlab-ee-arm64:${LATEST} ${{secrets.DOCKER_NAME}}/gitlab-ee-arm64:latest;
-    docker login --username ${{secrets.DOCKER_NAME}} --password ${{secrets.DOCKER_PASSWORD}} 
-    docker push -a ${{secrets.DOCKER_NAME}}/gitlab-ee-arm64
-    git config --local user.email ${{secrets.MAIL}}
-    git config --local user.name ${{secrets.MY_NAME}}
+    docker tag ${{DOCKER_NAME}}/gitlab-ee-arm64:${LATEST} ${DOCKER_NAME}/gitlab-ee-arm64:latest;
+    docker login --username ${secrets.DOCKER_NAME} --password ${DOCKER_PASSWORD} 
+    docker push -a ${secrets.DOCKER_NAME}/gitlab-ee-arm64
+    git config --local user.email ${MAIL}
+    git config --local user.name ${secrets.MY_NAME}
     git add latest
     git commit -m "build version ${LATEST}"
 fi
