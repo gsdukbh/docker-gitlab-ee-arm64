d4835c973a4400f921dccead6058e685bfa50efd
JiaWei Lee <gsdukbh@gmail.com>
2023-12-23 23:56:53 +0800

    fix: version

--- check-version.sh
+++ check-version.sh
@@ -7,7 +7,7 @@ if [ -f ./latest ]; then
 fi
 
 for i in $(seq 1 ${SEARCH_PAGES}); do
-    curl -s "https://packages.gitlab.com/gitlab/gitlab-ee?page=${i}" | grep "_arm64.deb" | grep -v '\-rc' | sed 's/.*>\(.*\)<.*/\1/' | sort -u | sed 's/gitlab-ee_\(.*\)_arm64.deb/\1/' >> latest;
+    curl -s "https://packages.gitlab.com/app/gitlab/gitlab-ee/search?filter=debs&page=${i}" | grep "_arm64.deb" | grep -v '\-rc' | sed 's/.*>\(.*\)<.*/\1/' | sort -u | sed 's/gitlab-ee_\(.*\)_arm64.deb/\1/' >> latest;
 done
 sort -rVu latest -o latest
 
