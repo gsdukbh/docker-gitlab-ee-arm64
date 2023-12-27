93a53b05b82a064926294b681bf0cae71980fa33
JiaWei <gsdukbh@gmail.com>
2023-12-27 14:49:46 +0800

    倒序版本

--- check-version.sh
+++ check-version.sh
@@ -9,5 +9,6 @@ fi
 for i in $(seq 1 ${SEARCH_PAGES}); do
     curl -s "https://packages.gitlab.com/app/gitlab/gitlab-ee/search?filter=debs&page=${i}" | grep "_arm64.deb" | grep -v '\-rc' | sed 's/.*>\(.*\)<.*/\1/' | sort -u | sed 's/gitlab-ee_\(.*\)_arm64.deb/\1/' >> latest;
 done
-sort -rVu latest -o latest
+sort -uV latest -o latest
+
 
