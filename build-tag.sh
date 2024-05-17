ef0ddcc3bb95abe767212e02f861d487d22fd508
JiaWei <gsdukbh@gmail.com>
2024-05-17 13:59:29 +0800

    fix

--- build-tag.sh
+++ build-tag.sh
@@ -2,9 +2,10 @@
     git clone https://gitlab.com/gitlab-org/omnibus-gitlab.git
     cd omnibus-gitlab/docker
     echo "PACKAGECLOUD_REPO=gitlab-ee" > RELEASE
+    echo "TARGETARCH=arm64" >> RELEASE
     echo "RELEASE_PACKAGE=gitlab-ee" >> RELEASE
     echo "RELEASE_VERSION=${LATEST}" >> RELEASE
-    echo "DOWNLOAD_URL=https://packages.gitlab.com/gitlab/gitlab-ee/packages/ubuntu/focal/gitlab-ee_${LATEST}_arm64.deb/download.deb" >> RELEASE
+    echo "DOWNLOAD_URL_arm64=https://packages.gitlab.com/gitlab/gitlab-ee/packages/ubuntu/focal/gitlab-ee_${LATEST}_arm64.deb/download.deb" >> RELEASE
     sed -i 's/\-recommends/\-recommends libatomic1/' Dockerfile
     docker run --privileged --rm tonistiigi/binfmt --install all
     docker buildx  build --platform linux/arm64 -t ${DOCKER_NAME}/gitlab-ee-arm64:${LATEST} -f Dockerfile ./
